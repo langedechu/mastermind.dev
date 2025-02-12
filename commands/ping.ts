f35321376040393e02ad23a22eda8adc5b4b775c
Annabelle KLENKLE <boncar.annabelle@gmail.com>
2025-02-12 13:33:08 +0100

    imp: embedded & response time

--- commands/ping.ts
+++ commands/ping.ts
@@ -1,9 +1,19 @@
-import type { CommandInteraction } from "discord.js";
+import { Colors, EmbedBuilder, type CommandInteraction } from "discord.js";
 
 export default {
   name: "ping",
   description: "Replies with 'Pong!'.",
   run(_interaction: CommandInteraction) {
-    _interaction.reply("Pong!");
+    _interaction.reply({
+      embeds: [
+        new EmbedBuilder({
+          title: ":ping_pong: Pong!",
+          description: `Response time: ${Math.abs(
+            Date.now() - _interaction.createdTimestamp
+          )}ms`,
+          color: Colors.Blurple,
+        }),
+      ],
+    });
   },
 };
